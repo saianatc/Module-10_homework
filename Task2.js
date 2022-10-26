0c5b804a583b899de9ecb44312c5e5b3991fa598
Saiana <111061312+saianatc@users.noreply.github.com>
2022-10-26 20:47:23 +0800

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn-test');
+btn.addEventListener('click', () => {
+        alert(`Screen Size - ` + window.screen.width + ` x ` + window.screen.height);
+    }
+);
